4b62dbd9719ed7ed6c29c1a2531ee1c96dee8f44
lyogics <lyogi@cps.edu>
2024-12-10 11:18:42 -0600

    finished

--- part4-classification/a6_part4.py
+++ part4-classification/a6_part4.py
@@ -8,7 +8,38 @@ data['Gender'].replace(['Male','Female'],[0,1],inplace=True)
 
 x = data[["Age", "EstimatedSalary", "Gender"]].values
 y = data["Purchased"].values
+print(x)
+print(y)
 
+scaler = StandardScaler().fit(x)
+x = scaler.transform(x)
+
+x_train, x_test, y_train, y_test = train_test_split(x, y)
+
+model = linear_model.LogisticRegression().fit(x_train, y_train)
+
+print("Accuracy:", model.score(x_test, y_test))
+print("*************")
+print("Testing Results:")
+print("")
+print(y_test)
+for index in range(len(x_test)):
+    x = x_test[index]
+    x = x.reshape(-1, 3)
+    print(x)
+    y_pred = int(model.predict(x))
+
+    if y_pred == 0:
+        y_pred = "Male"
+    elif y_pred == 1:
+        y_pred = "Female"
+    
+    actual = y_test[index]
+    if actual == 0:
+        actual = "Male"
+    elif actual == 1:
+        actual = "Female"
+    print("Predicted Gender: " + y_pred + " Actual Gender: " + actual)
 # Step 1: Print the values for x and y
 
 # Step 2: Standardize the data using StandardScaler, 
