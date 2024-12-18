6572fe4d70f3893920051d75ae8d4f54e2388588
lyogics <lyogi@cps.edu>
2024-12-18 11:44:29 -0600

    finished part 5

--- part5-unsupervised-learning/a5_part5.py
+++ part5-unsupervised-learning/a5_part5.py
@@ -8,8 +8,10 @@ data = pd.read_csv("part5-unsupervised-learning/customer_data.csv")
 x = data[["Annual Income", "Spending Score"]]
 
 #standardize the data
-x_std = StandardScaler().fit_transform(data)
+scaler = StandardScaler().fit(x)
+x_std = scaler.transform(x)
 
+x_orig = x_std * scaler.scale_ + scaler.mean_
 #the value of k has been defined for you
 k = 5
 #apply the kmeans algorithm
@@ -17,17 +19,18 @@ km = KMeans(n_clusters=k).fit(x_std)
 
 #get the centroid and label values
 centroids = km.cluster_centers_
+new_centroid = centroids * scaler.scale_ + scaler.mean_
 labels = km.labels_
 #sets the size of the graph
-plt.figure(figsize=(5,4))
+plt.figure(figsize=(10,8))
 
 #use a for loop to plot the data points in each cluster
 for i in range(k):
     cluster = x_std[labels == i]
-    plt.scatter(cluster[:,0], cluster[:,1], cluster[:,2], cluster[:,3], cluster[:,4])
+    plt.scatter(cluster[:,0], cluster[:,1])
 
 #plot the centroids
-plt.scatter(centroids[:, 0], centroids[:, 1], centroids[:,2], centroids[:,3], centroids[:,4], marker='X', s=100, c='r', label='centroid')
+plt.scatter(centroids[:, 0], centroids[:, 1], marker='X', s=100, c='r', label='centroid')
             
 #shows the graph
 plt.xlabel("Annual Income")
